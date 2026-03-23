698ee5f43f37f72117f4ceaad4a353d0f05c12c1
Fabian <fabian.urzua.b@gmail.com>
2026-03-23 17:27:32 -0300

    modificacion desarrollador fabito v2

--- HolaMundoService/Program.cs
+++ HolaMundoService/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/api", () => "Hello World 4 !");
+app.MapGet("/api", () => "Hello World 5 !");
 
 app.Run();
