d345669d48aa92a91577425952620fb527396b13
maub <maub89@gmail.com>
2026-03-23 17:01:05 -0300

    se modifica program.cs

--- HolaMundoService/Program.cs
+++ HolaMundoService/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/api", () => "Hello World 2 !");
+app.MapGet("/api", () => "Hello World 3 !");
 
 app.Run();
